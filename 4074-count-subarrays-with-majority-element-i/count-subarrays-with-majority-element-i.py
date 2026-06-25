98fc7e95cb587478472951e8ec7298d9cb0d6e4e
crocs8 <thakkararyan022@gmail.com>
2026-06-25 19:19:58 +0530

    Time: 15 ms (97.67%) | Memory: 12.6 MB (76.74%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
@@ -1,44 +1,17 @@
-class Fenwick:
-    def __init__(self, n):
-        self.bit = [0] * (n + 2)
-
-    def update(self, i, val):
-        while i < len(self.bit):
-            self.bit[i] += val
-            i += i & -i
-
-    def query(self, i):
-        s = 0
-        while i > 0:
-            s += self.bit[i]
-            i -= i & -i
-        return s
-
-
 class Solution(object):
     def countMajoritySubarrays(self, nums, target):
-        n = len(nums)
-
-        offset = n + 1
-        size = 2 * n + 3
-
-        bit = Fenwick(size)
-
-        prefix = 0
+        counts = defaultdict(int)
+        counts[0] = 1 
+        current_sum = 0
         ans = 0
-
-        bit.update(offset + 1, 1)
-
-        for x in nums:
-            if x == target:
-                prefix += 1
+        smaller_counts = 0
+        for num in nums:
+            if num == target:
+                smaller_counts += counts[current_sum]
+                current_sum += 1
             else:
-                prefix -= 1
-
-            idx = prefix + offset + 1
-
-            ans += bit.query(idx - 1)
-
-            bit.update(idx, 1)
-
+                current_sum -= 1
+                smaller_counts -= counts[current_sum]
+            ans += smaller_counts
+            counts[current_sum] += 1
         return ans
