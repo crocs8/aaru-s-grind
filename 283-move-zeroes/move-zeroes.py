8461f69925f6a03ae141c7bace86273fd49d8a81
crocs8 <thakkararyan022@gmail.com>
2026-07-02 13:18:04 +0530

    Time: 11 ms (26.37%) | Memory: 13.7 MB (19.25%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         j=0
-        for i in range(0,len(nums)):
+        for i in range(len(nums)):
             if(nums[i]!=0):
                 nums[j],nums[i]=nums[i],nums[j]
                 j+=1
