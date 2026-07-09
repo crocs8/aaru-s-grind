47995105fd0711fd502a5d3241bc537cb68c4f18
crocs8 <thakkararyan022@gmail.com>
2026-07-09 17:29:59 +0530

    Time: 99 ms (72.73%) | Memory: 45.3 MB (45.45%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.py
@@ -2,14 +2,12 @@ class Solution(object):
     def pathExistenceQueries(self, n, nums, maxDiff, queries):
         comp = [0] * n
         cid = 0
+        prev = nums[0]
 
         for i in range(1, n):
-            if nums[i] - nums[i - 1] > maxDiff:
+            if nums[i] - prev > maxDiff:
                 cid += 1
             comp[i] = cid
+            prev = nums[i]
 
-        ans = []
-        for u, v in queries:
-            ans.append(comp[u] == comp[v])
-
-        return ans
+        return [comp[u] == comp[v] for u, v in queries]
