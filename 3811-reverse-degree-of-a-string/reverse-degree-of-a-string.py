38d6902b47d36b0a61b163ec43aeb3f71a8bfbe1
crocs8 <thakkararyan022@gmail.com>
2026-09-20 15:56:08 +0530

    Time: 11 ms (20.35%) | Memory: 19.3 MB (55.57%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -1,11 +1,9 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
         #97 to 122
-        b=[]
+        c=0
         for i in range(len(s)):
             a=122-ord(s[i])+1
-            c=a*(i+1)
-            b.append(c)
-        print(b)
-        return sum(b)
+            c+=a*(i+1)
+        return c
     
